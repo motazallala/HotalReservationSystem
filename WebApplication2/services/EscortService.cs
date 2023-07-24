4579020a80fc879ffb425f73e9de0225140a66c5
Aizen <31359882+alaaalbayed@users.noreply.github.com>
2023-07-25 00:53:18 +0300

    add new service

--- WebApplication2/services/EscortService.cs
+++ WebApplication2/services/EscortService.cs
@@ -1,6 +1,51 @@
-﻿namespace WebApplication2.services
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication2.Data.Model;
+
+namespace WebApplication2.services
 {
     public class EscortService
     {
+        private readonly ApplicationDBContext _db;
+
+        public EscortService(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public async Task AddEscort(Escort escort)
+        {
+            await _db.Escorts.AddAsync(escort);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<List<Escort>> GetAllEscorts()
+        {
+            var allEscorts = await _db.Escorts.ToListAsync();
+            return allEscorts;
+        }
+
+        public async Task RemoveEscort(int Id)
+        {
+            var removeEscort = await _db.Escorts.FirstOrDefaultAsync(u => u.EscortId == Id);
+
+            if (removeEscort != null)
+            {
+                _db.Escorts.Remove(removeEscort);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task UpdateEscort(int Id, Escort escort)
+        {
+            escort.EscortId = Id;
+            var updateEscort = await _db.Escorts.FirstOrDefaultAsync(u => u.EscortId == Id);
+
+            if (updateEscort != null)
+            {
+                _db.Entry(updateEscort).CurrentValues.SetValues(escort);
+                await _db.SaveChangesAsync();
+            }
+        }
+
     }
 }
